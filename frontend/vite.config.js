369fda4abbbe51c4b5ffc4486a099731cb5e69a2
San Lin Htike <sanlinhtike@192.168.1.2>
2023-07-12 09:06:57 +0630

    ohh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://yo-back.vercel.app',
+        target: 'http://localhost:8000',
         changeOrigin: true,
       },
     },
